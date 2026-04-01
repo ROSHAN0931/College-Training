00f361dcfc4d3eee50da21cecc3e8087d4c2d951
Roshan Palaniappan <138195422+ROSHAN0931@users.noreply.github.com>
2026-04-01 14:04:05 +0530

    Add files via upload

---
+++ Saveetha/SaveethaTree/inclass/src/inclass3.java
@@ -0,0 +1,71 @@
+//import java.util.*;
+//
+//class TreeNode {
+//    int val;
+//    TreeNode left, right;
+//
+//    TreeNode(int val) {
+//        this.val = val;
+//        left = right = null;
+//    }
+//}
+//
+//public class Main {
+//
+//    // Build tree from level-order input
+//    public static TreeNode buildTree(String[] values) {
+//        if (values.length == 0 || values[0].equals("null"))
+//            return null;
+//
+//        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
+//        Queue<TreeNode> queue = new LinkedList<>();
+//        queue.add(root);
+//
+//        int i = 1;
+//
+//        while (!queue.isEmpty() && i < values.length) {
+//            TreeNode current = queue.poll();
+//
+//            // Left child
+//            if (i < values.length && !values[i].equals("null")) {
+//                current.left = new TreeNode(Integer.parseInt(values[i]));
+//                queue.add(current.left);
+//            }
+//            i++;
+//
+//            // Right child
+//            if (i < values.length && !values[i].equals("null")) {
+//                current.right = new TreeNode(Integer.parseInt(values[i]));
+//                queue.add(current.right);
+//            }
+//            i++;
+//        }
+//
+//        return root;
+//    }
+//
+//    // Function to find maximum depth
+//    public static int maxDepth(TreeNode root) {
+//        if (root == null) return 0;
+//
+//        int leftDepth = maxDepth(root.left);
+//        int rightDepth = maxDepth(root.right);
+//
+//        return 1 + Math.max(leftDepth, rightDepth);
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        int n = sc.nextInt();
+//        sc.nextLine();
+//
+//        String[] values = sc.nextLine().split(" ");
+//
+//        TreeNode root = buildTree(values);
+//
+//        int depth = maxDepth(root);
+//
+//        System.out.println(depth);
+//    }
+//}
